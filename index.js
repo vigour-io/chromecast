088678361e943a7c566fc67fb4f3728caefdaa80
Shawn Inder <shawn@vigour.io>
2015-04-12 18:52:23 +0200

    fixes

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 var bridge = require('vigour-native/lib/bridge')
+  , env = require('vigour-native/lib/env')
+  , name = "plugin"
+  , id = "vigour-native-plugin"
+  , supportedPlatforms = [
+    'android'
+    , 'ios'
+  ]
+  , supportedDevices = [
+    'phone'
+    , 'tablet'
+  ]
 
 module.exports = exports = {}
 
+exports.usable = ~supportedDevices.indexOf(env.ua.device)
+  && ~supportedPlatforms.indexOf(env.ua.platform)
+
 exports.act = function (opts, cb) {
-  if (opts.breaks) {
-    cb(new Error("Broke"))
-  } else {
-    cb(null, "Success")
+  var error
+  if (!cb) {
+    cb = opts
+    opts = null
   }
+  bridge.call(id, "act" ,opts, cb)
 }
