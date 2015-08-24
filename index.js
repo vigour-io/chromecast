b69f0652e5b1b2bd57b9fa312922ae27113615aa
Shawn Inder <shawn@vigour.io>
2015-08-24 10:54:04 +0200

    lint

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 var bridge = require('vigour-native/lib/bridge')
-  , env = require('vigour-native/lib/env')
-  , name = "plugin"
-  , id = "vigour-native-plugin"
-  , supportedPlatforms = [
-    'android'
-    , 'ios'
-  ]
-  , supportedDevices = [
-    'phone'
-    , 'tablet'
-  ]
+var env = require('vigour-native/lib/env')
+// var name = 'plugin'
+var id = 'vigour-native-plugin'
+var supportedPlatforms = [
+  'android',
+  'ios'
+]
+var supportedDevices = [
+  'phone',
+  'tablet'
+]
 
 module.exports = exports = {}
 
-exports.usable = ~supportedDevices.indexOf(env.ua.device)
-  && ~supportedPlatforms.indexOf(env.ua.platform)
+exports.usable = ~supportedDevices.indexOf(env.ua.device) &&
+  ~supportedPlatforms.indexOf(env.ua.platform)
 
 exports.act = function (opts, cb) {
-  var error
+  // var error
   if (!cb) {
     cb = opts
     opts = null
   }
-  bridge.call(id, "act" ,opts, cb)
+  bridge.call(id, 'act', opts, cb)
 }
