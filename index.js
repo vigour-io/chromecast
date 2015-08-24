fd980be3a4251e66157be907123e93c09e173dfe
Shawn Inder <shawn@vigour.io>
2015-08-24 15:34:47 +0200

    Call native initialization from js

--- index.js
+++ index.js
@@ -1,26 +1,34 @@
 var bridge = require('vigour-native/lib/bridge')
-var env = require('vigour-native/lib/env')
-// var name = 'plugin'
-var id = 'vigour-native-plugin'
-var supportedPlatforms = [
-  'android',
-  'ios'
-]
-var supportedDevices = [
-  'phone',
-  'tablet'
-]
+var pkg = require('./package.json')
+var pluginId = pkg.vigour.plugin.id
 
 module.exports = exports = {}
 
-exports.usable = ~supportedDevices.indexOf(env.ua.device) &&
-  ~supportedPlatforms.indexOf(env.ua.platform)
-
-exports.act = function (opts, cb) {
-  // var error
+exports.bridgeCall = function (fn, opts, cb) {
   if (!cb) {
     cb = opts
     opts = null
   }
-  bridge.call(id, 'act', opts, cb)
+  bridge.call(pluginId, fn, opts, cb)
+}
+
+// TODO Everything above this line feels like boilerplate
+// and should probably be moved to lib/bridge somehow
+
+// If plugin requires initialization on the native side
+exports.bridgeCall('init', function (err) {
+  if (!err) {
+    // Native part is ready
+    exports.init()
+  } else {
+    throw err
+  }
+})
+
+exports.init = function () {
+  // Do something
+}
+
+exports.act = function (opts, cb) {
+  exports.bridgeCall('act', opts, cb)
 }
