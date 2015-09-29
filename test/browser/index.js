e799df0ea01ae008bc2518db590c2890044dba4a
Shawn Inder <shawn@vigour.io>
2015-09-29 12:12:02 +0200

    Make tests that can work for every plugin

--- test/browser/index.js
+++ test/browser/index.js
@@ -1,11 +1,12 @@
 var Plugin = require('vigour-native/lib/bridge/Plugin')
+var name = require('../../package.json').name
 var plugin
 
 describe('plugin', function () {
   it('should be requireable', function () {
     plugin = require('../../')
     expect(plugin).instanceOf(Plugin)
-    expect(plugin.key).to.equal('plugin')
+    expect(plugin.key).to.equal(name)
   })
 
   describe('native events', function () {
@@ -13,7 +14,7 @@ describe('plugin', function () {
       var spy = sinon.spy()
       plugin.on('ready', spy)
       // Let's fake a ready event for this plugin
-      window.vigour.native.bridge.ready(null, 'message 1', plugin.key)
+      window.vigour.native.bridge.ready(null, 'message 1', name)
       window.vigour.native.bridge.ready(null, 'message 2')
       expect(spy).calledOnce
     })
@@ -22,7 +23,7 @@ describe('plugin', function () {
       var spy = sinon.spy()
       plugin.on('error', spy)
       // Let's fake a ready event for this plugin
-      window.vigour.native.bridge.error('message 1', plugin.key)
+      window.vigour.native.bridge.error('message 1', name)
       window.vigour.native.bridge.error('message 2')
       expect(spy).calledOnce
     })
@@ -31,7 +32,7 @@ describe('plugin', function () {
       var spy = sinon.spy()
       plugin.on('receive', spy)
       // Let's fake a ready event for this plugin
-      window.vigour.native.bridge.receive(null, 'message 1', plugin.key)
+      window.vigour.native.bridge.receive(null, 'message 1', name)
       window.vigour.native.bridge.receive(null, 'message 2')
       expect(spy).calledOnce
     })
