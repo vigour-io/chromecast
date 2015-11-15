5eb5aa985057ac217e4f107f23621a7de4a36b58
marcusbesjes <marcusbesjes@gmail.com>
2015-11-15 16:16:43 +0100

    work with val

--- test/browser/nativePlugin.js
+++ test/browser/nativePlugin.js
@@ -31,20 +31,15 @@ describe('Testing ChromeCast Native Plugin', function () {
   })
 
   describe('ChromeCast Sender', function () {
-
     var testDevice = {
       id: 'testDeviceId',
       name: 'testDeviceName'
     }
-    console.log('yes lala')
 
     it('should add devices passed through the \'join\' event', function () {
       // emulate device join
-      console.log('haha wut?')
       window.vigour.native.bridge.receive(null, {type: 'join', data: testDevice}, 'ChromeCast')
-      console.log('got here?')
       expect(plugin.devices[testDevice.id]).to.exists
-      console.log('got here as well?!!?!??')
     })
 
     it('should remove devices passed through the \'leave\' event', function () {
@@ -58,36 +53,35 @@ describe('Testing ChromeCast Native Plugin', function () {
       window.vigour.native.bridge.receive(null, {type: 'join', data: testDevice}, 'ChromeCast')
       // call startCasting for testDevice
       var device = plugin.devices[testDevice.id]
-      console.log('no trip?')
       plugin.startCasting(device)
 
-      // expect(plugin.session.val).to.equal(device)
-      // // session.val === device >> "connecting OR connected" to device
-      // expect(plugin.session).to.not.have.property('id')
-      // // state is now "connecting" because no session.id yet
-      //
-      // // wait for 'connected' event
-      // setTimeout(() => {
-      //   // expect 'connected' event to have fired 1 time
-      //   expect(plugin.session.id.val).to.exist
-      //   // state is now "connected" because I have a session id
-      //   done()
-      // }, 200)
+      expect(plugin.session.val).to.equal(device)
+      // session.val === device >> "connecting OR connected" to device
+      expect(plugin.session).to.not.have.property('id')
+      // state is now "connecting" because no session.id yet
+
+      // wait for 'connected' event
+      setTimeout(() => {
+        // expect 'connected' event to have fired 1 time
+        expect(plugin.session).to.exist
+          .and.to.have.property('id')
+          .which.has.property('val')
+          .which.ok
+        // state is now "connected" because I have a session id
+        done()
+      }, 200)
     })
-    //
-    // it('should be able to stop casting for a device', function (done) {
-    //   // device already joined
-    //   // call stopCasting for testDevice
-    //   plugin.stopCasting()
-    //   // session should be false
-    //   // wait for 'disconnected' event
-    //   setTimeout(() => {
-    //     expect(plugin.session).to.be.false
-    //     done()
-    //   }, 200)
-    // })
 
-
-
+    it('should be able to stop casting for a device', function (done) {
+      // device already joined
+      // call stopCasting for testDevice
+      plugin.stopCasting()
+      // session should be false
+      // wait for 'disconnected' event
+      setTimeout(() => {
+        expect(plugin.session.val).to.be.false
+        done()
+      }, 200)
+    })
   })
 })
