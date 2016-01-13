6ea49d33ceaabf7a79c4955a333a8b73d8ee80cd
Thomas Vervest <thomas@squarewolf.nl>
2016-01-13 18:41:37 +0100

    package and android

--- native/android/plugin/lib/src/main/java/io/vigour/plugin/chromecast/ChromecastPlugin.java
+++ native/android/plugin/lib/src/main/java/io/vigour/plugin/chromecast/ChromecastPlugin.java
@@ -5,13 +5,20 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.media.MediaRouteSelector;
 import android.support.v7.media.MediaRouter;
-import android.support.v7.media.RemotePlaybackClient;
+import android.support.v7.media.MediaRouter.RouteInfo;
+import android.util.Log;
 
 import com.fasterxml.jackson.jr.ob.impl.DeferredMap;
+import com.google.android.gms.cast.ApplicationMetadata;
 import com.google.android.gms.cast.Cast;
+import com.google.android.gms.cast.Cast.CastOptions;
 import com.google.android.gms.cast.CastDevice;
 import com.google.android.gms.cast.CastMediaControlIntent;
+import com.google.android.gms.cast.CastStatusCodes;
+import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -19,12 +26,15 @@ import java.util.Map;
 import io.vigour.nativewrapper.plugin.core.ActivityLifecycleListener;
 import io.vigour.nativewrapper.plugin.core.Plugin;
 
+import static com.google.android.gms.cast.Cast.CastApi;
+
 public class ChromecastPlugin extends Plugin implements ActivityLifecycleListener {
 	private static final String NAME = "chromecast";
 	private static final String TAG = ChromecastPlugin.class.getName();
 
 	public static final String ERROR_UNKNOWN_DEVICE = "unknown device";
 	public static final String ERROR_ALREADY_CASTING = "already casting to a device";
+	public static final String EVENT_STOPPED_CASTING = "stoppedCasting";
 
 	enum DeviceEvent {
 		DEVICE_JOINED("deviceJoined"),
@@ -45,17 +55,22 @@ public class ChromecastPlugin extends Plugin implements ActivityLifecycleListene
 	enum PluginStatus {
 		NEW,
 		READY,
-		PENDING_CAST,
+		PENDING_CONNECTION,
+		LAUNCHING_APP,
 		CASTING,
+		STOPPED,
+		SUSPENDED,
 	}
 
 	private final Context _context;
 	private final Map<String, MediaRouter.RouteInfo> _activeRoutes = new HashMap<>();
 	private final MediaRouter _mediaRouter;
 	private MediaRouteSelector _mediaRouteSelector;
-	private MediaRouterCallback _mediaRouterCallback;
+	private MediaRouter.Callback _mediaRouterCallback;
 	private PluginStatus _status = PluginStatus.NEW;
 	private boolean _isPendingCast = false;
+	private String _appId;
+	private GoogleApiClient _apiClient;
 
 	public ChromecastPlugin(@NonNull final Context context) {
 		super(NAME);
@@ -64,14 +79,29 @@ public class ChromecastPlugin extends Plugin implements ActivityLifecycleListene
 	}
 
 	public void init(@NonNull final DeferredMap settings) {
-		final String appId = (String) settings.get("appId");
+		if (_status != PluginStatus.NEW) {
+			throw new IllegalStateException("init should only be called once");
+		}
+
+		_appId = (String) settings.get("appId");
 
 		_mediaRouteSelector = new MediaRouteSelector.Builder()
-				//.addControlCategory(CastMediaControlIntent.categoryForRemotePlayback(appId))
-				.addControlCategory(CastMediaControlIntent.categoryForRemotePlayback(CastMediaControlIntent.DEFAULT_MEDIA_RECEIVER_APPLICATION_ID))
+				.addControlCategory(CastMediaControlIntent.categoryForRemotePlayback(_appId))
+				//.addControlCategory(CastMediaControlIntent.categoryForRemotePlayback(CastMediaControlIntent.DEFAULT_MEDIA_RECEIVER_APPLICATION_ID))
 				.build();
 
 		_status = PluginStatus.READY;
+	}
+
+	private void connectChannels() {
+		// TODO: reconnect after connection has ben lost and re-established
+	}
+
+	private void teardown() {
+		if (null != _apiClient) {
+			_apiClient.disconnect();
+			_apiClient = null;
+		}
 	}
 
 	public void startCasting(@NonNull final String deviceId) {
@@ -79,38 +109,118 @@ public class ChromecastPlugin extends Plugin implements ActivityLifecycleListene
 			throw new IllegalArgumentException(ERROR_UNKNOWN_DEVICE);
 		}
 
-		switch (_status) {
-			case CASTING:
-			case PENDING_CAST:
-				throw new IllegalStateException(ERROR_ALREADY_CASTING);
+		if (isCasting()) {
+			throw new IllegalStateException(ERROR_ALREADY_CASTING);
 		}
 
-		final MediaRouter.RouteInfo route = _activeRoutes.get(deviceId);
+		connectApiClient(_activeRoutes.get(deviceId));
+	}
+
+	private void connectApiClient(@NonNull final RouteInfo route) {
 		final CastDevice castDevice = CastDevice.getFromBundle(route.getExtras());
-		final Cast.CastOptions apiOptions = new Cast.CastOptions.Builder(castDevice, new CastListener(this)).build();
+		if (null == castDevice) {
+			return;
+		}
 
-		final GoogleApiClient apiClient = new GoogleApiClient.Builder(_context)
-				.addApi(Cast.API, apiOptions)
-				.addConnectionCallbacks(new GoogleApiClient.ConnectionCallbacks() {
-					@Override
-					public void onConnected(Bundle bundle) {
-						_status = PluginStatus.CASTING;
+		final Cast.Listener listener = new Cast.Listener() {
+			@Override
+			public void onApplicationDisconnected(int statusCode) {
+				ChromecastPlugin.this.onApplicationDisconnected(statusCode);
+			}
+
+			@Override
+			public void onApplicationStatusChanged() {
+				ChromecastPlugin.this.onApplicationStatusChanged();
+			}
+		};
+
+		final CastOptions apiOptions = new CastOptions.Builder(castDevice, listener).build();
+
+		_apiClient = new GoogleApiClient.Builder(_context)
+			.addApi(Cast.API, apiOptions)
+			.addConnectionCallbacks(new GoogleApiClient.ConnectionCallbacks() {
+				@Override
+				public void onConnected(Bundle bundle) {
+					if (PluginStatus.SUSPENDED == _status) {
+						connectChannels();
+					} else {
+						launchApplication();
 					}
+				}
 
-					@Override
-					public void onConnectionSuspended(int i) {
+				@Override
+				public void onConnectionSuspended(int i) {
+					_status = PluginStatus.SUSPENDED;
+					sendEvent(EVENT_STOPPED_CASTING, null);
+				}
+			})
+			.addOnConnectionFailedListener(new GoogleApiClient.OnConnectionFailedListener() {
+				@Override
+				public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+					if (isCasting()) {
+						sendEvent(EVENT_STOPPED_CASTING, null);
 					}
-				})
-				//.addOnConnectionFailedListener(mConnectionFailedListener)
-				.build();
+					teardown();
+				}
+			})
+			.build();
 
-		apiClient.connect();
-		_status = PluginStatus.PENDING_CAST;
+		_apiClient.connect();
+		_status = PluginStatus.PENDING_CONNECTION;
+	}
+
+	private void launchApplication() {
+		_status = PluginStatus.LAUNCHING_APP;
+		try {
+			CastApi.launchApplication(_apiClient, _appId)
+					.setResultCallback(new ResultCallback<Cast.ApplicationConnectionResult>() {
+						@Override
+						public void onResult(@NonNull Cast.ApplicationConnectionResult result) {
+							Status status = result.getStatus();
+							if (status.isSuccess()) {
+								_status = PluginStatus.CASTING;
+
+								/*
+								ApplicationMetadata applicationMetadata =
+										result.getApplicationMetadata();
+								String sessionId = result.getSessionId();
+								String applicationStatus = result.getApplicationStatus();
+								boolean wasLaunched = result.getWasLaunched();
+								*/
+							} else {
+								_status = PluginStatus.READY;
+								teardown();
+							}
+						}
+					});
+		} catch (Exception e) {
+			Log.e(TAG, "Failed to launch application", e);
+			throw e;
+		}
+	}
+
+	private boolean isCasting() {
+		if (null == _apiClient) {
+			return false;
+		}
+
+		switch (_status) {
+			case NEW:
+			case READY:
+			case STOPPED:
+				return false;
+		}
+
+		return true;
 	}
 
 	public void stopCasting() {
-		//final GoogleApiClient apiClient;
-		//apiClient.disconnect();
+		if (null == _apiClient) {
+			throw new IllegalStateException("app not casting");
+		}
+
+		_status = PluginStatus.STOPPED;
+		teardown();
 	}
 
 	public void onRouteAdded(@NonNull final MediaRouter.RouteInfo route) {
@@ -132,6 +242,75 @@ public class ChromecastPlugin extends Plugin implements ActivityLifecycleListene
 	}
 
 	public void onApplicationDisconnected(int statusCode) {
+		switch (statusCode) {
+			case CastStatusCodes.NETWORK_ERROR:
+				sendError("network error");
+				break;
+
+			case CastStatusCodes.TIMEOUT:
+				sendError("timeout");
+				break;
+
+			case CastStatusCodes.INTERRUPTED:
+				sendError("interrupted");
+				break;
+
+			case CastStatusCodes.AUTHENTICATION_FAILED:
+				sendError("authentication failed");
+				break;
+
+			case CastStatusCodes.INVALID_REQUEST:
+				sendError("invalid request");
+				break;
+
+			case CastStatusCodes.CANCELED:
+				sendError("canceled");
+				break;
+
+			case CastStatusCodes.NOT_ALLOWED:
+				sendError("not alllowed");
+				break;
+
+			case CastStatusCodes.APPLICATION_NOT_FOUND:
+				sendError("application not found");
+				break;
+
+			case CastStatusCodes.APPLICATION_NOT_RUNNING:
+				sendError("application not running");
+				break;
+
+			case CastStatusCodes.MESSAGE_TOO_LARGE:
+				sendError("message too large");
+				break;
+
+			case CastStatusCodes.MESSAGE_SEND_BUFFER_TOO_FULL:
+				sendError("message send buffer too full");
+				break;
+
+			case CastStatusCodes.FAILED:
+				sendError("failed");
+				break;
+
+			case CastStatusCodes.REPLACED:
+				sendError("replaced");
+				break;
+
+			case CastStatusCodes.INTERNAL_ERROR:
+				sendError("internal error");
+				break;
+
+			case CastStatusCodes.UNKNOWN_ERROR:
+				sendError("unknown error");
+				break;
+
+			case CastStatusCodes.ERROR_SERVICE_CREATION_FAILED:
+				sendError("error service creation failed");
+				break;
+
+			case CastStatusCodes.ERROR_SERVICE_DISCONNECTED:
+				sendError("error service disconnected");
+				break;
+		}
 	}
 
 	public void onApplicationStatusChanged() {
@@ -144,19 +323,36 @@ public class ChromecastPlugin extends Plugin implements ActivityLifecycleListene
 
 	@Override
 	public void onStart() {
-		_mediaRouterCallback = new MediaRouterCallback(this);
+		_mediaRouterCallback = new MediaRouter.Callback() {
+			@Override
+			public void onRouteAdded(MediaRouter router, MediaRouter.RouteInfo route) {
+				ChromecastPlugin.this.onRouteAdded(route);
+			}
+
+			@Override
+			public void onRouteRemoved(MediaRouter router, MediaRouter.RouteInfo route) {
+				ChromecastPlugin.this.onRouteRemoved(route);
+			}
+
+			//TODO: check with marcus on how to handle route changes
+			/*
+			@Override
+			public void onRouteChanged(MediaRouter router, MediaRouter.RouteInfo route) {
+				_plugin.sendDeviceEvent(DeviceEvent.DEVICE_JOINED, route.getId(), route.getName());
+				_plugin.sendDeviceEvent(DeviceEvent.DEVICE_LEFT, route.getId(), route.getName());
+			}
+			*/
+		};
 		_mediaRouter.addCallback(_mediaRouteSelector, _mediaRouterCallback,
 				MediaRouter.CALLBACK_FLAG_REQUEST_DISCOVERY);
 	}
 
 	@Override
 	public void onPause() {
-
 	}
 
 	@Override
 	public void onResume() {
-
 	}
 
 	@Override
